d8a4f076f7c7473ddfd7b52284f82f4fd212cb8a
alexandruvictor <andronachevictor@yahoo.com>
2021-03-22 23:19:04 +0200

    I added 3 cases in main ( setPerson, getPerson, exit)

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -27,6 +27,69 @@ public class Main {
 
             switch (option) {
 
+                case 1: {
+                    Scanner read = new Scanner(System.in).useDelimiter("\n"); /* Read until break line*/
+                    System.out.println("NAME: ");
+                    String name = read.next();
+
+                    System.out.println("CNP:");
+                    String CNP=read.next();
+
+                    personStub.setPerson(
+                            PersonOuterClass.Person.newBuilder()
+                                    .setName(name)
+                                    .setCNP(CNP).build(),
+                            new StreamObserver<PersonOuterClass.Empty>() {
+                                @Override
+                                public void onNext(PersonOuterClass.Empty empty) {
+
+                                }
+
+                                @Override
+                                public void onError(Throwable throwable) {
+                                    System.out.println("Error : " + throwable.getMessage());
+                                }
+
+                                @Override
+                                public void onCompleted() {
+
+                                }
+                            }
+                    );
+                    break;
+                }
+
+                case 2: {
+                    Scanner read = new Scanner(System.in).useDelimiter("\n");
+                    System.out.println("Name of person:");
+                    String name=read.next();
+
+                    personStub.getPerson(PersonOuterClass.PersonRequest.newBuilder().setName(name).build(),
+                            new StreamObserver<PersonOuterClass.PersonResponse>() {
+                                @Override
+                                public void onNext(PersonOuterClass.PersonResponse personResponse) {
+                                    System.out.println(personResponse);
+                                }
+
+                                @Override
+                                public void onError(Throwable throwable) {
+                                    System.out.println("Error : " + throwable.getMessage());
+                                }
+
+                                @Override
+                                public void onCompleted() {
+
+                                }
+                            });
+                    break;
+                }
+
+                case 3: {
+                    isConnected = false;
+                    break;
+                }
+                default:
+                    System.out.println("Unknown command, insert a valid command!");
             }
         }
         channel.shutdown();
